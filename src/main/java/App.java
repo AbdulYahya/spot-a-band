a17f319f110f0ba03b5b7c17bd66ce072a1f5a73
Guest User <Guest@Epicodus-5E.local>
2018-01-29 12:02:14 -0800

    Added 'profile/user' route. Added simple test logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,18 +14,41 @@ public class App {
         // Root - Index
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
+            /*
+            *   Below auth/user vars are used for testing header-nav only
+            * */
+            boolean authenticated = true;
+            String user = "Abdul";
+
+            if (authenticated) {
+                model.put("authenticated", authenticated);
+                model.put("user", user);
+            }
+
             return new HandlebarsTemplateEngine().render(new ModelAndView(model, "index.hbs"));
+        });
+
+        get("/signin", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "signin.hbs"));
+        });
+
+        get("/profile/:user", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String user = request.params("user");
+            boolean authenticated = true;
+            model.put("authenticated", authenticated);
+            model.put("user", user);
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "profile.hbs"));
         });
 
         // FILTERS
         before((request, response) -> {
             Map<String, Object> model = new HashMap<>();
-           boolean authenticated = false;
-
+            boolean authenticated = true;
             /*
             *  Check if user is Authenticated
             * */
-
             if (!authenticated) {
                 halt(401, new HandlebarsTemplateEngine().render(new ModelAndView(model, "signin.hbs")));
             }
