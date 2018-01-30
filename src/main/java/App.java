747a3e6908ff62509c5e9190af397b47806103d3
Guest User <Guest@Epicodus-5D.local>
2018-01-29 16:31:13 -0800

    update route notes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,8 @@
 
+import com.google.gson.Gson;
+import dao.Sql2oSpotifyDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -10,10 +14,29 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        Sql2oSpotifyDao sql2oSpotifyDao;
+        Connection conn;
+        Gson gson = new Gson();
+        String connectionString = "jdbc:h2:~/spot-a-band.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        sql2oSpotifyDao = new Sql2oSpotifyDao(sql2o);
+        conn = sql2o.open();
 
         // Root - Index
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "index.hbs"));
+        });
+
+        //get top artist
+        get("https://api.spotify.com/v1/me/top/artists", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+
+            //big JSON goes here
+            //artists = json.queryParams("artists");
+            // a miracle occurs to turn artists into an array
+            //
+
             return new HandlebarsTemplateEngine().render(new ModelAndView(model, "index.hbs"));
         });
 
