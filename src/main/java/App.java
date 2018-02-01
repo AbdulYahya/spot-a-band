cd08634fedb7772226bd472692220281fd8e1dbe
Aaron Ross and James Donlan <aaron@>
2018-02-01 10:48:13 -0800

    make some css changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -93,7 +93,6 @@ public class App {
             String username = spotifyDao.formatUserId(user.getId()).toString();
             //System.out.println(user.getImages());
 
-
             if (user.getDisplayName() != null) {
                 model.put("user", user.getDisplayName());
                 request.session().attribute("user", user.getDisplayName());
